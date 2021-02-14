64b857ab0eb7840da67ccabcac46ac1ea3c6de8f
MassicardLouis <louis.massicard@orange.fr>
2021-02-14 15:02:40 +0100

    Complete more informations in core

--- src/main/java/fr/sid/miage/dicegameCharlesMassicard/core/Player.java
+++ src/main/java/fr/sid/miage/dicegameCharlesMassicard/core/Player.java
@@ -1,5 +1,9 @@
 package fr.sid.miage.dicegameCharlesMassicard.core;
 
+import java.util.logging.Logger;
+
+import fr.ul.miage.louis.chantiersimulator.Camion;
+
 /**
  * @author Louis MASSICARD (user name : louis)
  * @version 
@@ -10,14 +14,52 @@ public class Player {
 
 	/* ========================================= Global ================================================ */ /*=========================================*/
 
+	private static final Logger LOG = Logger.getLogger(Player.class.getName());
+	
 	/* ========================================= Attributs ============================================= */ /*=========================================*/
+
+	private String name;
+
+	private int score;
 
 	/* ========================================= Constructeurs ========================================= */ /*=========================================*/
 
+	public Player(String name) {
+		this.setName(name);
+		this.setScore(0);
+	}
+	
 	/* ========================================= Methodes ============================================== */ /*=========================================*/
 
 	/* ========================================= Accesseurs ============================================ */ /*=========================================*/
 
-	/* ========================================= Main ================================================== */ /*=========================================*/
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
 
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return the score
+	 */
+	public int getScore() {
+		return score;
+	}
+
+	/**
+	 * @param score the score to set
+	 */
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	/* ========================================= Main ================================================== */ /*=========================================*/
 }
