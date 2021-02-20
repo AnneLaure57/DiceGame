ec95ce79ebb619d07f95ceda0aefd20fd4866ed2
MassicardLouis <louis.massicard@orange.fr>
2021-02-20 17:44:03 +0100

    HighScorePostGreSQL : suppr unused imports.

--- src/main/java/fr/sid/miage/dicegameCharlesMassicard/persist/HighScorePostGreSQL.java
+++ src/main/java/fr/sid/miage/dicegameCharlesMassicard/persist/HighScorePostGreSQL.java
@@ -1,9 +1,7 @@
 package fr.sid.miage.dicegameCharlesMassicard.persist;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import fr.sid.miage.dicegameCharlesMassicard.core.Entry;
 import fr.sid.miage.dicegameCharlesMassicard.core.HighScore;
