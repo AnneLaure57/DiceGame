0af73ef68b5acf347215aadf74c19b669fac0655
MassicardLouis <louis.massicard@orange.fr>
2021-02-20 18:16:53 +0100

    init TestHighScorePostGreSQL

---
+++ src/main/java/fr/sid/miage/dicegameCharlesMassicard/tests/TestHighScorePostGreSQL.java
@@ -0,0 +1,53 @@
+package fr.sid.miage.dicegameCharlesMassicard.tests;
+
+import fr.sid.miage.dicegameCharlesMassicard.persist.HighScoreXML;
+import fr.sid.miage.dicegameCharlesMassicard.persist.PersistKit;
+import fr.sid.miage.dicegameCharlesMassicard.persist.XMLKit;
+
+/**
+ * @author Anne-Laure CHARLES
+ * @author Louis MASSICARD (user name : louis)
+ * @version 
+ * @since %G% - %U% (%I%)
+ *
+ *
+ * Temp class to test backend : HighScorePostGreSQL.
+ */
+public class TestHighScorePostGreSQL {
+	/* ========================================= Global ================================================ */ /*=========================================*/
+
+	/* ========================================= Attributs ============================================= */ /*=========================================*/
+
+	/* ========================================= Constructeurs ========================================= */ /*=========================================*/
+
+	/* ========================================= Methodes ============================================== */ /*=========================================*/
+
+	/* ========================================= Accesseurs ============================================ */ /*=========================================*/
+
+	/* ========================================= Main ================================================== */ /*=========================================*/
+	public static void main(String[] args) {
+		
+		// Declarations
+		PersistKit persistKit;
+		HighScoreXML highScore;
+
+		// Initialisations
+		persistKit = new XMLKit();
+		highScore = (HighScoreXML) persistKit.makeKit();
+		
+		// Use it
+		highScore.add("Louis", 100);
+		highScore.add("AL", 100);
+		highScore.add("Claire", 50);
+		highScore.add("Adel", 0);
+		
+		// Display
+		highScore.getScores().forEach(System.out::println);
+		
+		// Test save
+		highScore.save();
+		
+		// Test load
+		highScore.load();
+	}
+}
