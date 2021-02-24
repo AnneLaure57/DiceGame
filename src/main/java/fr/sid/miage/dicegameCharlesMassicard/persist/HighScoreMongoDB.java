b43441e6ccb41f2eb3d5bf1f98b587c471094cc2
MassicardLouis <louis.massicard@orange.fr>
2021-02-24 19:14:54 +0100

    HighScoreMongoDB : comment undo method and stop using deprecated methods.

--- src/main/java/fr/sid/miage/dicegameCharlesMassicard/persist/HighScoreMongoDB.java
+++ src/main/java/fr/sid/miage/dicegameCharlesMassicard/persist/HighScoreMongoDB.java
@@ -9,8 +9,15 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.bson.Document;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
 
 import fr.sid.miage.dicegameCharlesMassicard.core.Entry;
 import fr.sid.miage.dicegameCharlesMassicard.core.HighScore;
@@ -44,18 +51,17 @@ public class HighScoreMongoDB implements HighScore {
 	 */
 	private static HighScoreMongoDB INSTANCE = null;
 	
-	// JDBC driver name and database URL
-	private static final String JDBC_DRIVER = "org.postgresql.Driver";  
-	private static final String SERVER_URL = "jdbc:postgresql://localhost:5432/";
+	// Name and database URL
+	private static final String SERVER_URL = "mongodb://localhost:27017";
 	private static final String DATABASE_NAME = "dicegame";
 	private static final String DATABASE_URL = SERVER_URL + DATABASE_NAME;
 
 	//  Database credentials
-	private static final String DATABASE_USER = "postgres";
-	private static final String DATABASE_PASS = "riovas";
+//	private static final String DATABASE_USER = "postgres";
+//	private static final String DATABASE_PASS = "riovas";
 
-	// Tables informations
-	private static final String TABLE_NAME = "ENTRIES";
+	// Collection's informations
+	private static final String COLLECTION_NAME = "ENTRIES";
 	private static final String TABLE_FIELD_ID = "ID";
 	private static final String TABLE_FIELD_NAME = "NAME";
 	private static final String TABLE_FIELD_SCORE = "SCORE";
@@ -142,7 +148,21 @@ public class HighScoreMongoDB implements HighScore {
 		LOG.info("Check connection to database : " + DATABASE_URL);
 				    
 	    try {
-	    	MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+	    	MongoClient mongoClient = new MongoClient(new MongoClientURI(SERVER_URL));
+	    	// Deprecated
+//	    	DB database = mongoClient.getDB(DATABASE_NAME);
+//	    	List<String> databases = mongoClient.getDatabaseNames();
+//	    	for (String string : databases) {
+//				System.out.println("DB name : " + string);
+//			}
+//	    	DBCollection collection = database.getCollection(COLLECTION_NAME);
+	    	
+	    	MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
+	    	MongoIterable<String> collectionNames = database.listCollectionNames();
+	    	MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
+	    	
+	    	// Uncomment if you want more details
+	    	// this.displayExistingDatabaseNames(mongoClient);
 	    	
 	    	LOG.info("MongoDB : Opened database successfully");
 	    	
@@ -152,6 +172,13 @@ public class HighScoreMongoDB implements HighScore {
 	    	System.exit(0);
 	    }
 	}
+	
+//	private void displayExistingDatabaseNames (MongoClient mongoClient) {
+//		MongoIterable<String> databaseNames = mongoClient.listDatabaseNames();
+//    	for (String string : databaseNames) {
+//    		System.out.println("DB name : " + string);
+//    	}
+//	}
 
 	/**
 	 * Method createTableIfNotExists : to run the command CREATE TABLE IF NOT EXISTS.
@@ -163,40 +190,40 @@ public class HighScoreMongoDB implements HighScore {
 	 *  * otherwise : docker start postgres
 	 */
 	public void createTableIfNotExists () {
-		LOG.info("Create table (if not exists), table name : " + TABLE_NAME);
-		
-		Connection connection = null;
-		Statement statement = null;
-		int commandReturn;
-	    	    
-	    try {
-	    	Class.forName(JDBC_DRIVER);
-	    	connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASS);
-	    	statement = connection.createStatement();
-	    	
-	    	String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME +
-	                " ("+ TABLE_FIELD_ID + " INT PRIMARY KEY NOT NULL," +
-	                " " + TABLE_FIELD_NAME + " TEXT NOT NULL, " +
-	                " " + TABLE_FIELD_SCORE + " INT NOT NULL);";
-	    	commandReturn = statement.executeUpdate(sql);
-	    		    	
-	    	if (commandReturn == 0) {
-				LOG.info("SQL statement return nothing.");
-			} else {
-				LOG.info("The row count for SQL Data Manipulation Language (DML) statements.");
-				LOG.info("Row count : " + commandReturn);
-			}
-	    				    	
-	    	statement.close();
-	    	connection.close();
-	    	
-	    	LOG.info("PostGreSQL : the table " + TABLE_NAME + " is ready.");
-	    	
-	    } catch (Exception error) {
-	    	error.printStackTrace();
-	    	LOG.severe(error.getClass().getName() + ": " + error.getMessage());
-	    	System.exit(0);
-	    }
+//		LOG.info("Create table (if not exists), table name : " + TABLE_NAME);
+//		
+//		Connection connection = null;
+//		Statement statement = null;
+//		int commandReturn;
+//	    	    
+//	    try {
+//	    	Class.forName(JDBC_DRIVER);
+//	    	connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASS);
+//	    	statement = connection.createStatement();
+//	    	
+//	    	String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME +
+//	                " ("+ TABLE_FIELD_ID + " INT PRIMARY KEY NOT NULL," +
+//	                " " + TABLE_FIELD_NAME + " TEXT NOT NULL, " +
+//	                " " + TABLE_FIELD_SCORE + " INT NOT NULL);";
+//	    	commandReturn = statement.executeUpdate(sql);
+//	    		    	
+//	    	if (commandReturn == 0) {
+//				LOG.info("SQL statement return nothing.");
+//			} else {
+//				LOG.info("The row count for SQL Data Manipulation Language (DML) statements.");
+//				LOG.info("Row count : " + commandReturn);
+//			}
+//	    				    	
+//	    	statement.close();
+//	    	connection.close();
+//	    	
+//	    	LOG.info("PostGreSQL : the table " + TABLE_NAME + " is ready.");
+//	    	
+//	    } catch (Exception error) {
+//	    	error.printStackTrace();
+//	    	LOG.severe(error.getClass().getName() + ": " + error.getMessage());
+//	    	System.exit(0);
+//	    }
 	}
 	
 	/**
@@ -209,73 +236,37 @@ public class HighScoreMongoDB implements HighScore {
 	 *  * otherwise : docker start postgres
 	 */
 	public void truncateTable () {
-		LOG.info("Truncate table, table name : " + TABLE_NAME);
-		
-		Connection connection = null;
-		Statement statement = null;
-		int commandReturn;
-	    	    
-	    try {
-	    	Class.forName(JDBC_DRIVER);
-	    	connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASS);
-	    	statement = connection.createStatement();
-	    	
-	    	String sql = "TRUNCATE " + TABLE_NAME + ";";
-	    	commandReturn = statement.executeUpdate(sql);
-	    		    	
-	    	if (commandReturn == 0) {
-				LOG.info("SQL statement return nothing.");
-			} else {
-				LOG.info("The row count for SQL Data Manipulation Language (DML) statements.");
-				LOG.info("Row count : " + commandReturn);
-			}
-	    				    	
-	    	statement.close();
-	    	connection.close();
-	    	
-	    	LOG.info("PostGreSQL : the table " + TABLE_NAME + " is truncated, so now the table is empty.");
-	    	
-	    } catch (Exception error) {
-	    	error.printStackTrace();
-	    	LOG.severe(error.getClass().getName() + ": " + error.getMessage());
-	    	System.exit(0);
-	    }
-	}
-	
-	/**
-	 * Method insertOne : to insert a unique Entry (with decuplate params).
-	 * 
-	 * @param ID The entry's ID.
-	 * @param name The entry's player's name.
-	 * @param score The entry's player's score.
-	 */
-	public void insertOne (int ID, String name, int score) {
-		LOG.info("Insert One into table : " + TABLE_NAME);
-		
-		Connection connection = null;
-		Statement statement = null;
-			    	    
-	    try {
-	    	Class.forName(JDBC_DRIVER);
-	    	connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASS);
-	    	connection.setAutoCommit(false);
-	    	
-	    	statement = connection.createStatement();
-	    	String sql = "INSERT INTO " + TABLE_NAME + " (" + TABLE_FIELD_ID + "," + TABLE_FIELD_NAME + "," + TABLE_FIELD_SCORE + ") "
-	    			+ "VALUES (" + ID + ", " + name + ", " + score +");";
-	    	statement.executeUpdate(sql);
-	    		    	
-	    	statement.close();
-	    	connection.commit();
-	    	connection.close();
-	    	
-	    	LOG.info("PostGreSQL : the Entry is inserted.");
-	    	
-	    } catch (Exception error) {
-	    	error.printStackTrace();
-	    	LOG.severe(error.getClass().getName() + ": " + error.getMessage());
-	    	System.exit(0);
-	    }
+//		LOG.info("Truncate table, table name : " + TABLE_NAME);
+//		
+//		Connection connection = null;
+//		Statement statement = null;
+//		int commandReturn;
+//	    	    
+//	    try {
+//	    	Class.forName(JDBC_DRIVER);
+//	    	connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASS);
+//	    	statement = connection.createStatement();
+//	    	
+//	    	String sql = "TRUNCATE " + TABLE_NAME + ";";
+//	    	commandReturn = statement.executeUpdate(sql);
+//	    		    	
+//	    	if (commandReturn == 0) {
+//				LOG.info("SQL statement return nothing.");
+//			} else {
+//				LOG.info("The row count for SQL Data Manipulation Language (DML) statements.");
+//				LOG.info("Row count : " + commandReturn);
+//			}
+//	    				    	
+//	    	statement.close();
+//	    	connection.close();
+//	    	
+//	    	LOG.info("PostGreSQL : the table " + TABLE_NAME + " is truncated, so now the table is empty.");
+//	    	
+//	    } catch (Exception error) {
+//	    	error.printStackTrace();
+//	    	LOG.severe(error.getClass().getName() + ": " + error.getMessage());
+//	    	System.exit(0);
+//	    }
 	}
 	
 	/**
@@ -284,37 +275,37 @@ public class HighScoreMongoDB implements HighScore {
 	 * @param scores The list of Entry to insert in database/table.
 	 */
 	public void insertMany (List<Entry> scores) {
-		LOG.info("Insert Many into table : " + TABLE_NAME);
-		
-		Connection connection = null;
-		Statement statement = null;
-			    	    
-	    try {
-	    	Class.forName(JDBC_DRIVER);
-	    	connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASS);
-	    	connection.setAutoCommit(false);
-	    	
-	    	statement = connection.createStatement();
-	    	
-	    	int index = 0;
-	    	for (Entry entry : scores) {
-	    		index++;
-	    		String sql = "INSERT INTO " + TABLE_NAME + " (" + TABLE_FIELD_ID + "," + TABLE_FIELD_NAME + "," + TABLE_FIELD_SCORE + ") "
-	    				+ "VALUES (" + index + ", " + "'"+entry.getName()+"'" + ", " + entry.getScore() +");";
-	    		statement.executeUpdate(sql);
-			}
-	    		    	
-	    	statement.close();
-	    	connection.commit();
-	    	connection.close();
-	    	
-	    	LOG.info("PostGreSQL : the list of Entry is inserted.");
-	    	
-	    } catch (Exception error) {
-	    	error.printStackTrace();
-	    	LOG.severe(error.getClass().getName() + ": " + error.getMessage());
-	    	System.exit(0);
-	    }
+//		LOG.info("Insert Many into table : " + TABLE_NAME);
+//		
+//		Connection connection = null;
+//		Statement statement = null;
+//			    	    
+//	    try {
+//	    	Class.forName(JDBC_DRIVER);
+//	    	connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASS);
+//	    	connection.setAutoCommit(false);
+//	    	
+//	    	statement = connection.createStatement();
+//	    	
+//	    	int index = 0;
+//	    	for (Entry entry : scores) {
+//	    		index++;
+//	    		String sql = "INSERT INTO " + TABLE_NAME + " (" + TABLE_FIELD_ID + "," + TABLE_FIELD_NAME + "," + TABLE_FIELD_SCORE + ") "
+//	    				+ "VALUES (" + index + ", " + "'"+entry.getName()+"'" + ", " + entry.getScore() +");";
+//	    		statement.executeUpdate(sql);
+//			}
+//	    		    	
+//	    	statement.close();
+//	    	connection.commit();
+//	    	connection.close();
+//	    	
+//	    	LOG.info("PostGreSQL : the list of Entry is inserted.");
+//	    	
+//	    } catch (Exception error) {
+//	    	error.printStackTrace();
+//	    	LOG.severe(error.getClass().getName() + ": " + error.getMessage());
+//	    	System.exit(0);
+//	    }
 	}
 	
 	/**
@@ -323,42 +314,42 @@ public class HighScoreMongoDB implements HighScore {
 	 * @return The list of Entry to inserted in database/table.
 	 */
 	public List<Entry> getMany () {
-		LOG.info("Get Many from table : " + TABLE_NAME);
+		LOG.info("Get Many from table : " + COLLECTION_NAME);
 		
 		List<Entry> scores = new ArrayList<Entry>();
-		
-		Connection connection = null;
-		Statement statement = null;
-		ResultSet resultSet = null;
-			    	    
-	    try {
-	    	Class.forName(JDBC_DRIVER);
-	    	connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASS);
-	    	connection.setAutoCommit(false);
-	    	
-	    	statement = connection.createStatement();
-	    	resultSet = statement.executeQuery("SELECT * FROM " + TABLE_NAME + ";");
-	    	
-	    	while (resultSet.next()) {
-	    		int ID = resultSet.getInt("id");
-	            String  name = resultSet.getString("name");
-	            int score  = resultSet.getInt("score");
-	            System.out.println( "ID = " + ID );
-	            System.out.println( "NAME = " + name );
-	            System.out.println( "SCORE = " + score );
-	            System.out.println();
-	        }
-	    		    	
-	        resultSet.close();
-	        statement.close();
-	        connection.close();
-	    	
-	    } catch (Exception error) {
-	    	error.printStackTrace();
-	    	LOG.severe(error.getClass().getName() + ": " + error.getMessage());
-	    	System.exit(0);
-	    }
-	    
+//		
+//		Connection connection = null;
+//		Statement statement = null;
+//		ResultSet resultSet = null;
+//			    	    
+//	    try {
+//	    	Class.forName(JDBC_DRIVER);
+//	    	connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASS);
+//	    	connection.setAutoCommit(false);
+//	    	
+//	    	statement = connection.createStatement();
+//	    	resultSet = statement.executeQuery("SELECT * FROM " + TABLE_NAME + ";");
+//	    	
+//	    	while (resultSet.next()) {
+//	    		int ID = resultSet.getInt("id");
+//	            String  name = resultSet.getString("name");
+//	            int score  = resultSet.getInt("score");
+//	            System.out.println( "ID = " + ID );
+//	            System.out.println( "NAME = " + name );
+//	            System.out.println( "SCORE = " + score );
+//	            System.out.println();
+//	        }
+//	    		    	
+//	        resultSet.close();
+//	        statement.close();
+//	        connection.close();
+//	    	
+//	    } catch (Exception error) {
+//	    	error.printStackTrace();
+//	    	LOG.severe(error.getClass().getName() + ": " + error.getMessage());
+//	    	System.exit(0);
+//	    }
+//	    
 	    return scores;
 	}
 		
