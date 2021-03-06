2e2163b17f426a116cda97f8c4c9cae20d8eb1cb
MassicardLouis <louis.massicard@orange.fr>
2021-03-06 20:17:10 +0100

    RollDices : add Javadoc.

--- src/main/java/fr/sid/miage/dicegameCharlesMassicard/utils/strategy/RollDices.java
+++ src/main/java/fr/sid/miage/dicegameCharlesMassicard/utils/strategy/RollDices.java
@@ -26,6 +26,15 @@ public class RollDices implements RollStrategy{
 
 	/* ========================================= Methodes ============================================== */ /*=========================================*/
 	
+	/**
+	 * Method rollDices : roll dice using a specific strategy.
+	 * Die one rolls first, then Die two rolls 3 seconds after dice one.
+	 * 
+	 * @param die1 The first die used for the Dice Game.
+	 * @param die2 The second die used for the Dice Game.
+	 * 
+	 * @return Return true if you succed to roll dice, otherwise return false.
+	 */
 	@Override
 	public boolean rollDices(Die die1, Die die2) {
 		try {
