d3c0cab5b4faa229b2472f840b1470861e8e0c15
MassicardLouis <louis.massicard@orange.fr>
2021-03-06 17:53:53 +0100

    RollStrategy : add Javadoc.

--- src/main/java/fr/sid/miage/dicegameCharlesMassicard/utils/strategy/RollStrategy.java
+++ src/main/java/fr/sid/miage/dicegameCharlesMassicard/utils/strategy/RollStrategy.java
@@ -7,12 +7,18 @@ import fr.sid.miage.dicegameCharlesMassicard.core.Die;
  * @author Louis MASSICARD (user name : louis)
  * @version 
  * @since %G% - %U% (%I%)
- *
+ * 
+ * The strategy to use to roll dice.
  */
-
 public interface RollStrategy {
 	
-	//Name of the strategy method
+	/**
+	 * Method rollDices : roll dice using a specific strategy.
+	 * 
+	 * @param die1 The first die used for the Dice Game.
+	 * @param die2 The second die used for the Dice Game.
+	 * 
+	 * @return Return true if you succed to roll dice, otherwise return false.
+	 */
 	public boolean rollDices (Die die1, Die die2);
-
 }
